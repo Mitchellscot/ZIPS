427ee3495093825ed525351a87225631ed9275b4
mitchellscot <Mitchellscott@me.com>
2021-02-25 07:38:39 -0600

    started Admin

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import './Admin.css';
+import React, { useState } from 'react';
+import {useSelector} from 'react-redux';
+
+return function Admin(){
+    return(
+        <>
+        </>
+    );
+}
+
+export default Admin;
---
+++ src/components/AdminTableRow/AdminTableRow.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import {useSelector} from 'react-redux';
+import './AdminTableRow.css';
+
+
+function AdminTableRow(props) {
+
+  const store = useSelector((store) => store);
+
+
+  return (
+
+    );
+}
+
+export default AdminTableRow;
