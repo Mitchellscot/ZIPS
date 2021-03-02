0bef5dd777df1b6f9566f1670902080210cc93f6
mitchellscot <Mitchellscott@me.com>
2021-03-02 16:06:55 -0600

    routing works!

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -4,22 +4,43 @@ import Col from 'react-bootstrap/Col';
 import AdminHeader from '../AdminHeader/AdminHeader';
 import AdminSideBar from '../AdminSideBar/AdminSideBar';
 import Row from 'react-bootstrap/Row';
-import Nav from 'react-bootstrap/Nav';
 import OrdersTable from '../OrdersTable/OrdersTable';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+} from "react-router-dom";
+import PicturesTable from '../PicturesTable/PicturesTable';
+import Camera from '../CameraSettings/CameraSettings';
+import Account from '../AccountSettings/AccountSettings';
 
 function Admin() {
-
+    let match = useRouteMatch();
     return (
         <>
             <AdminHeader />
             <Container fluid>
                 <Row>
                     <AdminSideBar />
-                    <Col md={10} lg={11} className="ml-sm-auto px-md-5 py-5">
-                    <main className="d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border border-bottom-0 rounded-top">
-                        
-                        <OrdersTable />
-                        
+                    <Col md={10} lg={11} className="ml-sm-auto px-md-5 py-3">
+                        <main className="d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border border-bottom-0 rounded-top">
+                        <Switch>
+                        <Route path="/Admin/orders">
+                            <OrdersTable />
+                            </Route>
+                        <Route path="/Admin/pictures">
+                            <PicturesTable />
+                            </Route>
+                        <Route path="/Admin/Camera">
+                            <Camera />
+                        </Route>
+                        <Route path="/Admin/Account">
+                            <Account />
+                        </Route>
+                        </Switch>
                         </main>
                     </Col>
                 </Row>
---
+++ src/components/PicturesTable/PicturesTable.jsx
@@ -0,0 +1,9 @@
+import './PicturesTable.css';
+
+function PicturesTable(){
+    return(
+        <h1>PICTURES</h1>
+    );
+}
+
+export default PicturesTable;
