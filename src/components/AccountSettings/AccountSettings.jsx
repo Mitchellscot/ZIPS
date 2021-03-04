cc4f7523d11fb5267ce48701331155793d1d6d27
mitchellscot <Mitchellscott@me.com>
2021-03-04 12:35:57 -0600

    begining to add an account page

--- src/components/AccountSettings/AccountSettings.jsx
+++ src/components/AccountSettings/AccountSettings.jsx
@@ -1,8 +1,22 @@
 import './AccountSettings.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
-function AccountSettings(){
-    return(
-        <h1>ACCOUNT</h1>
+function AccountSettings() {
+    return (
+        <Container fluid>
+                <Form>
+                    <h4>Email Settings</h4>
+                <Form.Group controlId="exampleForm.ControlInput1" className="d-flex justify-content-between">
+                        <Form.Label className="fluid">
+                        Sending Email
+                        </Form.Label>
+                        <Form.Control className="mx-auto" type="email" placeHolder="bztinfo@ziplinemn.com" />
+                    </Form.Group>
+                </Form>
+        </Container >
     );
 }
 
